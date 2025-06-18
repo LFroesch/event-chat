7958519e16f5070b827b10a00a89b2300e028313
LFroesch <lucas.froeschner@gmail.com>
2025-06-17 20:04:05 -0700

    frontend semi-working

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -57,10 +58,10 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    // const receiverSocketId = getReceiverSocketId(receiverId);
-    // if (receiverSocketId) {
-    //   io.to(receiverSocketId).emit("newMessage", newMessage);
-    // }
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,15 +5,15 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
+import { app, server } from "./lib/socket.js";
 
 // .env and Config
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173"],
   credentials: true
 }));
 app.use(express.json({limit: '10mb'}));
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   connectDB();
 })
